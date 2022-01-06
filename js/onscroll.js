c5eb3882e4e44c9dc6a941c648e8119180c41c61
Francesco <francesco.magnani14@studio.unibo.it>
2022-01-06 21:31:46 +0100

    refactoring of all css: first phase

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,9 +1,8 @@
 let nav = document.getElementsByClassName("navbar-bot")[0];
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        $(".navbar-bot").fadeOut(300);
-    }
-    else {
-        $(".navbar-bot").fadeIn(200);
-    }
- });
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+    $(".navbar-bot").fadeOut(300);
+  } else {
+    $(".navbar-bot").fadeIn(200);
+  }
+});
