e020acee1a389d2cac63f1b2ba5fb94a5e172a8b
CavDev26 <lorenzo.cavallucci4@studio.unibo.it>
2022-01-16 12:30:23 +0100

    FIrst implementation of php

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 define("UPLOAD_DIR", "./upload/");
-require_once("utils/functions.php");
 require_once("db/database.php");
-$dbh = new DatabaseHelper("localhost", "root", "", "oniric", 3307);
+$dbh = new DatabaseHelper("localhost", "root", "", "ecommerce", 3307);
 ?>
